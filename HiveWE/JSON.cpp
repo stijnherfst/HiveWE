777d8b7e7b3d6b068158884c0f8a79adceecfb46
Bogdan Gligorijevic <bogdangligorijevic3@gmail.com>
2018-08-12 14:16:54 +0200

    Added conversion to lowercase

--- HiveWE/JSON.cpp
+++ HiveWE/JSON.cpp
@@ -24,7 +24,8 @@ namespace json {
 					}
 					if (line.substr(0, 12) == "    {\"src\":\"") {
 						end1 = line.find('\"', 13);
-						const std::string key = line.substr(12, end1 - 12);
+						std::string key = line.substr(12, end1 - 12);
+						std::transform(key.begin(), key.end(), key.begin(), ::tolower);
 						// If the segment already exists
 						if (json_data.count(key)) {
 							continue;
@@ -51,10 +52,12 @@ namespace json {
 	}
 
 	bool JSON::exists(std::string file) const {
-	#ifdef _DEBUG
+		std::string file_lower_case = file;
+		std::transform(file_lower_case.begin(), file_lower_case.end(), file_lower_case.begin(), ::tolower);
+		#ifdef _DEBUG
 		std::cout << "Queried existance of alias for: " << file;
-	#endif
-		if (json_data.count(file))
+		#endif
+		if (json_data.count(file_lower_case))
 		{
 			#ifdef _DEBUG
 			std::cout << "  FOUND\n";
@@ -68,6 +71,8 @@ namespace json {
 	}
 
 	std::string JSON::alias(std::string file) const {
-		return json_data.at(file);
+		std::string file_lower_case = file;
+		std::transform(file_lower_case.begin(), file_lower_case.end(), file_lower_case.begin(), ::tolower);
+		return json_data.at(file_lower_case);
 	}
 }
